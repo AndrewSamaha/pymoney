d39139ee09a30851dcb828b8a878cc2be7606ec8
Andrew Samaha <andrew.samaha@gmail.com>
2023-05-17 19:14:22 -0400

    fix(data-pipeline): Correct for balance type in openOneCsv

--- data-pipeline/app/helpers/readCsv.py
+++ data-pipeline/app/helpers/readCsv.py
@@ -25,6 +25,11 @@ def openOneCsv(path):
     date_columns = getDateColumns(path)
     print(f"reading {path} with these date columns: {date_columns}")
     df = pd.read_csv(path, index_col=False, parse_dates=date_columns)
+    if "Balance" in df.columns:
+        print(" setting Balance to numeric")
+        df["Balance"] = pd.to_numeric(df["Balance"], errors="coerce")
+    print(f"column types: {df.dtypes}")
+    print(f". all columns={df.columns}")
     df[hashType] = df.apply(hash, axis=1)
     df['stagingLevel'] = 'allTransactions'
     
