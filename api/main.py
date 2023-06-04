0b0c2201309bf4357bac9c12b33938080da3bdc7
Andrew Samaha <andrew.samaha@gmail.com>
2023-06-04 14:10:07 -0400

    feat(api): adds summary route

--- api/main.py
+++ api/main.py
@@ -3,11 +3,13 @@
 from fastapi import FastAPI
 from app.routes.all_transactions import router as api_router
 from app.routes.health_check import router as health_check_router
+from app.routes.summary import router as summary_router
 
 app = FastAPI()
 
 app.include_router(health_check_router, prefix="")
 app.include_router(api_router, prefix="/api/v1")
+app.include_router(summary_router, prefix="/api/v1")
 
 if __name__ == "__main__":
     uvicorn.run(
