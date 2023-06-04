60a7bdf81af9049fcdf0c973ce26aecdd0e4fc69
Andrew Samaha <andrew.samaha@gmail.com>
2023-06-03 23:49:27 -0400

    chore(dbwrapper): cleans up comments

--- dbwrapper/dbwrapper/helpers/query_to_df.py
+++ dbwrapper/dbwrapper/helpers/query_to_df.py
@@ -1,7 +1,5 @@
 import pandas as pd
-#from dbwrapper.helpers.readCsv import setDateColumns
 from . import setDateColumns
-#from dbwrapper.helpers.readCsv import setDateColumns
 
 def query_to_df(query, convert_date_columns=True):
     result = query.all()
