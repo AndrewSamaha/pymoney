9944d9bdac6d397a4db66e4a39e82555e1ff8b7c
Andrew Samaha <andrew.samaha@gmail.com>
2023-07-03 17:19:39 -0400

    chore(dbwrapper): Cleans up unused files

--- dbwrapper/tests/constants/mockDb.py
+++ dbwrapper/tests/constants/mockDb.py
@@ -1,7 +1,3 @@
 mockdbfilepath = 'mock_data/money.sqlite'
 rawCsvPath = 'mock_data/rawTransactionHistory'
 mockConnectionString = f"sqlite:///{mockdbfilepath}"
-
-# dbfilepath = 'mock_data/money.sqlite'
-# rawCsvPath = '../db/rawTransactionHistory'
-# connectionString = f"sqlite:///{dbfilepath}"
